cf4f28a81f2103359532f850c5f3ac1aec85ea84
stefan_karlberg <stefan.karlberg@smackit.se>
2019-11-17 08:02:06 +0100

    all files

---
+++ test/hs/scriptloader/516902.js
@@ -0,0 +1,4 @@
+(function(id,src){if(document.getElementById(id)){return;}
+var js=document.createElement('script');js.src=src;js.type='text/javascript';js.id=id;var e=document.getElementsByTagName('script')[0];e.parentNode.insertBefore(js,e);})('hs-analytics','//js.hs-analytics.net/analytics/1573930500000/516902.js');(function(id,src,attrs){if(document.getElementById(id)){return;}
+var js=document.createElement('script');js.src=src;js.type='text/javascript';js.id=id;for(var name in attrs){if(attrs.hasOwnProperty(name)){js.setAttribute(name,attrs[name]);}}
+var e=document.getElementsByTagName('script')[0];e.parentNode.insertBefore(js,e);})('hubspot-messages-loader','https://js.usemessages.com/conversations-embed.js',{"data-loader":"hs-scriptloader","data-hsjs-portal":516902,"data-hsjs-env":"prod"});
